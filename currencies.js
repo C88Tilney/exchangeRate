4c121b9b7ab690b72c2a324bcfbeb668ab02c65b
C88Tilney <CT@c-macbook.home>
2016-11-18 16:53:06 +0000

    add html small exchange table
    added £ exchange table that run as soon as the page is loads.
    
    also added the value user currency choice compare to USD And Eur

--- currencies.js
+++ currencies.js
@@ -1,3 +1,28 @@
+
+//event listeners on page load
+window.addEventListener("load", gbpRate, false);
+
+//GBP exchange rate on the load of page
+function gbpRate() {
+    var xmlhttp = new XMLHttpRequest();
+    var gbpBaseRate = "http://api.fixer.io/latest?base=gbp";
+
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var gbp = JSON.parse(this.responseText);
+            console.log(gbp);
+            
+            var usd = gbp.rates.USD;
+            var eur = gbp.rates.EUR;
+            console.log(eur);
+            document.getElementById("dollar").innerHTML = usd;
+            document.getElementById("euro").innerHTML = eur;
+        }
+    };
+    xmlhttp.open("GET", gbpBaseRate, true);
+    xmlhttp.send();
+};
+
 //ADD EVENT LISTENERS
 window.addEventListener("load", start, false);
 
@@ -28,14 +53,24 @@ function loadData() {
             var dis = myArr.rates[toCurr];
             console.log(dis);
             var calculation = dis * fromAmount;
+            var UserUsd = myArr.rates.USD;
+            var UserEur = myArr.rates.EUR;
             console.log(calculation);
+            document.getElementById("toAmount").value = calculation;
 
+            //other curreny rate
+            document.getElementById("userChoice").innerHTML = "1 " + fromCurr + " = ";
+            
+            console.log(UserEur);
+            document.getElementById("userUs").innerHTML = UserUsd + " USD";
+            document.getElementById("userEur").innerHTML = UserEur + " EUR";
 
-            document.getElementById("demo").innerHTML = calculation;
         };
     };
     xmlhttp.open("GET", url, true);
     xmlhttp.send();
 
-
+  
 };
+
+
